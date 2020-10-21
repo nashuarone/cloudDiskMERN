bdd11fa704bcabe70d06a5908bacc6347cbcc37d
nashuarone <nashuarash@gmail.com>
2020-10-22 00:38:07 +0500

    22 oct 2020 - create user directory (err need fix)

--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -2,10 +2,12 @@ const Router = require('express')
 const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
 const {check, validationResult} = require('express-validator')
-const User = require('../models/user')
+const User = require('../models/User')
 const config = require('config')
 const router = new Router()
 const authMiddleware = require('../middleware/auth.middleware')
+const fileService = require('../services/fileService')
+const File = require('../models/File')
 
 router.post('/registration', [
   check('email', "Uncorrect email").isEmail(),
@@ -28,6 +30,7 @@ router.post('/registration', [
     const hashPassword = await bcrypt.hash(password, 8)
     const user = new User({email, password: hashPassword})
     await user.save()
+    await fileService.createDir(new File({user: user.id, name: ''}))
     return res.json({message: "User was created"})
 
   } catch (e) {
